f0db87b7a2349cb9e19fa01f06ada4707749a76d
hhhhack <cy@4-ad0036.localdomain>
2020-11-05 15:34:09 +0800

    提交更改

--- ReplaceSpace.go
+++ ReplaceSpace.go
@@ -1,10 +1,10 @@
 package leetcode
 
-import "fmt"
+//import "fmt"
 
 func ReplaceSpace(src string)string{
 	var tmp = make([]byte, 0)
-	space := []byte{'%'. '2', '0'}
+	space := []byte{'%', '2', '0'}
 	for idx, val := range(src){
 		if val == ' '{
 			tmp = append(tmp, space...)
