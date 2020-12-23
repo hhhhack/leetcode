a9bfce273db90dadaee5ccf484d5011cd28b06c5
hhhhack <1791743233@qq.com>
2020-12-23 17:21:43 +0800

    删除多余文件

--- 算法导论/mergeSort.cpp
+++ 算法导论/mergeSort.cpp
@@ -25,7 +25,7 @@
  * 
  * @Author: hhhhack
  * @Date: 2020-12-16 10:51:44
- * @LastEditTime: 2020-12-16 11:39:40
+ * @LastEditTime: 2020-12-18 16:26:44
  * @LastEditors: hhhhack
  * @Description: 
  * @FilePath: /code/leetcode/算法导论/mergeSort.cpp
@@ -38,16 +38,10 @@ using namespace std;
 void merge(vector<int> &input, int i, int k, int j){
     vector<int> a(k - i + 1);
     vector<int> b(j - k);
-    //cout << "i  is "<< i << "j is  "<< j << "k is "<< k << "\n"; 
-    //cout<<"i is "<<i<<"j is "<< j << "k is "<<k << endl;
-    // a[k - i + 1] = -1;
-    // b[j - k] = -1;
     for(int m = i; m <= k; m++){
-        //cout << "m is " << m << " \n";
         a[m - i] = input[m];
     }
     for (int n = k + 1; n <= j; n++){
-        //cout << "n is " << n << " \n";
         b[n - k - 1] = input[n];
     }
     int cur = i, cur_a = 0, cur_b = 0;
