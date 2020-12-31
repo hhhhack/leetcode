8056fc766dd88288458cb581ae66b7122b42fa7c
hhhhack <1791743233@qq.com>
2020-12-31 17:34:34 +0800

    12-31提交

--- 博客/go-gin/log.go
+++ 博客/go-gin/log.go
@@ -24,41 +24,11 @@
  *
  *
  * @Author: hhhhack
- * @Date: 2020-12-30 09:50:33
- * @LastEditTime: 2020-12-31 17:15:07
+ * @Date: 2020-12-31 17:00:10
+ * @LastEditTime: 2020-12-31 17:00:13
  * @LastEditors: hhhhack
  * @Description:
- * @FilePath: /code/leetcode/博客/go-gin/main.go
+ * @FilePath: /code/leetcode/博客/go-gin/log.go
  * @
  */
 
-package main
-
-import (
-	"flag"
-	"fmt"
-	"net/http"
-	"os"
-
-	"gopkg.in/ini.v1"
-)
-
-var ConfigFile string
-
-func init() {
-	Config := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-	Config.StringVar(&ConfigFile, "c", "/etc/myapp.ini", "config file path")
-	Config.Parse()
-	if Config.Parsed() {
-		cfg, err := ini.Load(ConfigFile)
-		if err != nil {
-			log.Printf("load config file fail %v ", err)
-			panic("load config file fail")
-		}
-	}
-	if cfg.Section("LOG").
-}
-
-func main() {
-	server := http.Server{}
-}
