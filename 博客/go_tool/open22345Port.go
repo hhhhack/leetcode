804db58e821898556938f3f595308dcb473ba0ba
hhhhack <1791743233@qq.com>
2021-01-13 19:25:05 +0800

    1-13提交

--- 博客/go_tool/open22345Port.go
+++ 博客/go_tool/open22345Port.go
@@ -25,62 +25,25 @@
  *
  * @Author: hhhhack
  * @Date: 2021-01-09 11:34:13
- * @LastEditTime: 2021-01-09 11:34:16
+ * @LastEditTime: 2021-01-13 14:41:07
  * @LastEditors: hhhhack
  * @Description:
  * @FilePath: /code/leetcode/博客/go_tool/open22345Port.go
  * @
  */
 
-/*
- *
- * 　　┏┓　　　┏┓+ +
- * 　┏┛┻━━━┛┻┓ + +
- * 　┃　　　　　　　┃
- * 　┃　　　━　　　┃ ++ + + +
- *  ████━████ ┃+
- * 　┃　　　　　　　┃ +
- * 　┃　　　┻　　　┃
- * 　┃　　　　　　　┃ + +
- * 　┗━┓　　　┏━┛
- * 　　　┃　　　┃
- * 　　　┃　　　┃ + + + +
- * 　　　┃　　　┃
- * 　　　┃　　　┃ +  神兽保佑
- * 　　　┃　　　┃    代码无bug
- * 　　　┃　　　┃　　+
- * 　　　┃　 　　┗━━━┓ + +
- * 　　　┃ 　　　　　　　┣┓
- * 　　　┃ 　　　　　　　┏┛
- * 　　　┗┓┓┏━┳┓┏┛ + + + +
- * 　　　　┃┫┫　┃┫┫
- * 　　　　┗┻┛　┗┻┛+ + + +
- *
- *
- * @Author: hhhhack
- * @Date: 2020-09-10 17:29:25
- * @LastEditTime: 2021-01-08 16:31:13
- * @LastEditors: hhhhack
- * @Description:
- * @FilePath: \undefinedd:\project\go\open22345port.go
- * @
- */
-
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 	"sync"
 	"time"
@@ -88,13 +51,6 @@ import (
 
 	"gopkg.in/ini.v1"
 )
-
-type deviceInfo struct {
-	userName  string
-	password  string
-	ipadderss string
-	httpport  int
-}
 
 var (
 	configFile *string = nil
@@ -235,50 +191,6 @@ GETCOOKIE:
 	return 0
 }
 
-func parseConf(conf string) deviceInfo {
-	if len(conf) == 0 {
-		log.Printf("conf is nil")
-		return deviceInfo{"", "", ""}
-	}
-	if strings.Index(conf, ":") != -1 {
-		confs := strings.Split(conf, ":")
-		fmt.Println("conf is :", confs)
-		if len(confs) != 3 {
-			log.Printf("conf is illagal")
-			return deviceInfo{"", "", ""}
-		}
-		return deviceInfo{strings.TrimSpace(confs[0]), strings.TrimSpace(confs[1]), strings.TrimSpace(confs[2])}
-	}
-	return deviceInfo{"admin", "admin", strings.TrimSpace(conf)}
-}
-
-func getConfig(configs []deviceInfo) int {
-	f, err := os.Open(*configFile)
-	if err != nil {
-		fmt.Printf("open file fail")
-		return -1
-	}
-	defer f.Close()
-	reader := bufio.NewReader(f)
-	for {
-		line, err := reader.ReadString('\n') //注意是字符
-		//fmt.Printf("read line is %s \n", line)
-		if err == io.EOF {
-			if len(line) != 0 {
-				configs = append(configs, parseConf(line))
-			}
-			fmt.Println("文件读完了")
-			break
-		}
-		if err != nil {
-			fmt.Println("read file failed, err:", err)
-			return -1
-		}
-		configs = append(configs, parseConf(line))
-	}
-	return 0
-}
-
 func main() {
 	if (ipAdderss == nil || *ipAdderss == "") && (configFile == nil || *configFile == "") {
 		fmt.Printf("argument error, please use -h for help")
@@ -287,7 +199,7 @@ func main() {
 	configs := make([]deviceInfo, 0)
 	//configs = append(configs, {userName, password, ipAdderss})
 	if configFile != nil {
-		if getConfig(configs) == -1 {
+		if parseConf.getConfig(configs) == -1 {
 			log.Printf("read file fail")
 			return
 		}
