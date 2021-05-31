a2267dfc5817d7b6218e892fcaa6ad962053d11c
hhhhack <1791743233@qq.com>
2021-05-31 11:17:05 +0800

    5-31提交2

--- 博客/go_permission_tool/xsl/WriteExecl.go
+++ 博客/go_permission_tool/xsl/WriteExecl.go
@@ -25,7 +25,7 @@
  *
  * @Author: hhhhack
  * @Date: 2021-05-27 17:54:07
- * @LastEditTime: 2021-05-27 20:13:33
+ * @LastEditTime: 2021-05-31 11:16:55
  * @LastEditors: hhhhack
  * @Description:
  * @FilePath: /leetcode/博客/go_permission_tool/xsl/WriteExecl.go
@@ -43,8 +43,15 @@ import (
 
 func WritExecl(xmlRet *myxml.XmlRet) {
 	f := excelize.NewFile()
+	var style excelize.Style
+	style.Font = new(excelize.Font{"blod":true, "color":""})
+	ret, err := f.NewStyle(style interface{})
 	for _, permission := range xmlRet.Permission {
 		sheet := f.NewSheet(permission.Name)
+		f.SetColStyle(sheet, "B", ret) error
+
+		f.SetCellBool(sheet, axis string, value bool) error
+		f.SetCellValue()
 
 		f.SetCellValue(permission.Name, "A1", permission.PerName[0].Data)
 	}
